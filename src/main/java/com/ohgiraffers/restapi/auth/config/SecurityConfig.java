536e1722eb7c6e36635ec0f45a9c22a8354f795e
macacolabs <monkey.ohgiraffers@gmail.com>
2026-08-11 13:08:07 +0900

    config modify

--- src/main/java/com/ohgiraffers/restapi/auth/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/restapi/auth/config/SecurityConfig.java
@@ -93,7 +93,6 @@ public class SecurityConfig {
         config.addAllowedOrigin("http://localhost:3001");
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://bucket-seungje.s3-website.ap-northeast-2.amazonaws.com");
-        config.addAllowedOrigin("https://api.naengpa.com");
         config.addAllowedOrigin("https://www.naengpa.com");
         config.addAllowedOrigin("https://naengpa.com");
         config.addAllowedHeader("*");
