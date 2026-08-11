c0a801f9d96cc19d37cc03fde5e930838804d959
macacolabs <monkey.ohgiraffers@gmail.com>
2026-08-11 12:09:04 +0900

    origin add

--- src/main/java/com/ohgiraffers/restapi/auth/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/restapi/auth/config/SecurityConfig.java
@@ -93,6 +93,7 @@ public class SecurityConfig {
         config.addAllowedOrigin("http://localhost:3001");
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://bucket-seungje.s3-website.ap-northeast-2.amazonaws.com");
+        config.addAllowedOrigin("https://api.naengpa.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
